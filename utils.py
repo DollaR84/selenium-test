e98471ec8f904d861c5f066738006f33371bfb96
DollaR <cooler84@ukr.net>
2021-08-05 13:37:26 +0300

    create functions for create ansert email and count letters and digits in message

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 """
-Generator module for test task with email.
+Utils module for test task with email.
 
 Created on 04.08.2021
 
@@ -11,6 +11,27 @@ import random
 import string
 
 
+TEMPLATE_EMAIL = """
+Received mail on theme {0} with message: {1}. It contains {2} letters and {3} numbers
+"""
+
+
 def get_string(length):
     symbols = string.ascii_letters + string.digits
     return ''.join(random.sample(symbols, length))
+
+
+def create_email(emails):
+    """Create answer email with all input emails."""
+    answer_list = []
+    for subject, message in emails.items():
+        answer_list.append(TEMPLATE_EMAIL.format(subject, message, count_letters(message), count_digits(message)))
+    return '\n'.join(answer_list)
+
+
+def count_letters(message):
+    return sum([1 for s in message if s.isalpha()])
+
+
+def count_digits(message):
+    return sum([1 for s in message if s.isdigit()])
